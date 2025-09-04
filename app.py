1dcc4d9e3d13e98ed2cf6949218b2cde6e8443b7
Ralph <rorozco@salvi.es>
2025-09-04 12:49:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -267,7 +267,7 @@ with tab2:
         min_lon, max_lon = df["Longitude - Functional Location"].min(), df["Longitude - Functional Location"].max()
         min_lat, max_lat = df["Latitude - Functional Location"].min(), df["Latitude - Functional Location"].max()
     
-        lon_margin = (max_lon - min_lon) * 0.1  # 10% margen extra
+        lon_margin = (max_lon - min_lon) * 0.5  # 10% margen extra
         lat_margin = (max_lat - min_lat) * 0.5
     
         ax.set_xlim(min_lon - lon_margin, max_lon + lon_margin)
